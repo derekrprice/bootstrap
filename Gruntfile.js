af4f17e6b79ae1c229d4d0a69ccde2e67f7b0de8
Derek Price <derek.price@hp.com>
2014-01-27 16:41:44 -0500

    chore: Updated dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,8 +16,8 @@ module.exports = function(grunt) {
   grunt.util.linefeed = '\n';
 
   grunt.initConfig({
-    ngversion: '1.0.5',
-    bsversion: '3.0.0-rc2',
+    ngversion: '1.2.4',
+    bsversion: '3.0.0',
     modules: [],//to be filled in by build task
     pkg: grunt.file.readJSON('package.json'),
     dist: 'dist',
